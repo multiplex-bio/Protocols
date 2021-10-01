2e90dbab6cd433d4b725e85ec8953280f9d2550b
bpvalderrama <bvalderrama@multiplex.bio>
2021-10-01 17:32:38 -0300

    Improved instructions for the user

--- TNA/carga_de_rx_en_TNA.py
+++ TNA/carga_de_rx_en_TNA.py
@@ -1,6 +1,6 @@
-print("Con este script vamos a calcular la cantidad de reactivos que son necesarios para realizar el protocolo de Extracción de TNA con Opentrons.")
+print("\nCon este script vamos a calcular la cantidad de reactivos que son necesarios para realizar el protocolo de Extracción de TNA con Opentrons.")
 
-sample_number = int(input("Ingrese la cantidad de muestras que desea procesar: "))
+sample_number = int(input("\nIngrese la cantidad de muestras que desea procesar: "))
 
 confirm_sample_number = input('Confirmas que vas a usar {} muestras (y/n)'.format(sample_number)).lower()
 confirm_sample_number = confirm_sample_number.lower()
@@ -10,7 +10,7 @@ while confirm_sample_number != "y":
     confirm_sample_number = input('Confirmas que vas a usar {} muestras (usa y/n para responder)'.format(sample_number)).lower()
     confirm_sample_number = confirm_sample_number.lower()
 
-vol_total_beads = (30 * sample_number * 1)/1000 # uL usados para cada muestra * Cantidad de muestras * Cantidad de veces que se usan las beads / 1000 para tener el volumen en mL
+vol_total_beads = (50 * sample_number * 1)/1000 # uL usados para cada muestra * Cantidad de muestras * Cantidad de veces que se usan las beads / 1000 para tener el volumen en mL
 vol_total_etanol = (130 * sample_number * 2)/1000 # uL usados para lavar cada muestra * Cantidad de muestras * Cantidad de lavados para cada muestra / 1000 para tener el volumen en mL
 vol_total_etanol_sal = (130 * sample_number * 1)/1000 # uL usados para lavar cada muestra * Cantidad de muestras * Cantidad de lavados para cada muestra / 1000 para tener el volumen en mL
 vol_total_elution_buffer = (60 * sample_number * 1)/1000 # uL usados para lavar cada muestra * Cantidad de muestras * Cantidad de lavados para cada muestra / 1000 para tener el volumen en mL
@@ -27,9 +27,26 @@ wells_etanol_sal = math.ceil(vol_total_etanol_sal/max_vol_reservoir)
 wells_elution_buffer = math.ceil(vol_total_elution_buffer/max_vol_reservoir)
 wells_descarte = math.ceil(vol_total_descarte/max_vol_reservoir)
 
-print('Para llevar a cabo el protocolo necesitarás ...')
-print('{} mL de beads. Tienes que cargar {} wells'.format(vol_total_beads, wells_beads))
-print('{} mL de etanol. Tienes que cargar {} wells'.format(vol_total_etanol, wells_etanol))
-print('{} mL de etanol sal. Tienes que cargar {} wells'.format(vol_total_etanol_sal, wells_etanol_sal))
-print('{} mL de agua ultra pura. Tienes que cargar {} wells'.format(vol_total_elution_buffer, wells_elution_buffer))
-print('{} mL de descarte. Tienes que dejar disponibles {} wells'.format(vol_total_descarte, wells_descarte))
+print('\n\nPara llevar a cabo el protocolo ... \n')
+
+print("Usarás {} mL de beads. Tienes que cargar {} well, en la posición 'A1' del reservoir\n".format(vol_total_beads+1, wells_beads)) #+1 porque se vio experimentalmente que son necesarios para mejorar la precision del pipeteo
+
+if wells_etanol > 1:
+    print("Usarás {} mL de etanol. Tienes que cargar {} mL en las posiciones 'A2' y 'A3' del reservoir\n".format(vol_total_etanol, vol_total_etanol/wells_etanol))
+else:
+    print("Usarás {} mL de etanol. Tienes que cargar {} mL en la posición 'A2' del reservoir\n".format(vol_total_etanol, vol_total_etanol/wells_etanol))
+
+if wells_etanol > 1:    
+    print("Usarás {} mL de etanol sal. Tienes que cargar {} mL en la posición 'A4'\n".format(vol_total_etanol_sal, vol_total_etanol_sal/wells_etanol_sal))
+else:
+    print("Usarás {} mL de etanol sal. Tienes que cargar {} mL en la posición 'A3'\n".format(vol_total_etanol_sal, vol_total_etanol_sal/wells_etanol_sal))
+
+if wells_etanol > 1:    
+    print("Usarás {} mL de agua ultra pura. Tienes que cargar {} mL en la posición 'A5'\n".format(vol_total_elution_buffer, vol_total_elution_buffer/wells_elution_buffer))
+else:
+    print("Usarás {} mL de agua ultra pura. Tienes que cargar {} mL en la posición 'A4'\n".format(vol_total_elution_buffer, vol_total_elution_buffer/wells_elution_buffer))
+
+if wells_descarte > 1:
+    print("Usarás {} mL de descarte. Tienes que dejar disponibles {} wells,".format(vol_total_descarte, wells_descarte) + " desde el 'A12' hasta el 'A" + str(12-wells_descarte) + "'\n")
+else:
+    print("Usarás {} mL de descarte. Tienes que dejar disponibles {} well, en la posición 'A12'\n".format(vol_total_descarte, wells_descarte))
