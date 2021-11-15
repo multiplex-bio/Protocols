7b452869ba13ea61ddbc667918151075aa64373e
bpvalderrama <bvalderrama@multiplex.bio>
2021-11-15 12:28:39 -0300

    dispensing RNA samples more close to the bottom and faster flow_rates for mixing

--- RT/RT_prep.py
+++ RT/RT_prep.py
@@ -13,7 +13,7 @@ metadata = {
 
 def get_values(*names):
     # los valores para que las variables custom_* funcione son "yes" o "no"
-    _all_values = json.loads("""{"sample_number":8,"custom_tipracks":"yes", "custom_sample_plate":"no", "custom_output_plate":"yes"}""")
+    _all_values = json.loads("""{"sample_number":8,"custom_tipracks":"no", "custom_sample_plate":"no", "custom_output_plate":"no"}""")
     return [_all_values[n] for n in names]
 
 def run(protocol):
@@ -29,10 +29,10 @@ def run(protocol):
     slots = ['4','5','6']  
     
     if custom_tipracks == 'yes':
-        tipracks = [protocol.load_labware('vertex_96_tiprack_200ul', slot, 'small tips tiprack') for slot in slots]
+        tipracks = [protocol.load_labware('vertex_96_tiprack_200ul', slot, 'tiprack') for slot in slots]
         
     else:
-        tipracks = [protocol.load_labware('opentrons_96_tiprack_20ul', slot, 'small tips tiprack') for slot in slots]
+        tipracks = [protocol.load_labware('opentrons_96_tiprack_20ul', slot, 'tiprack') for slot in slots]
     
     
     
@@ -78,23 +78,33 @@ def run(protocol):
     # RT - PASO 1: Primers + H20
     vol_primers_h2o = sample_number*3 # Se necesitan 3 uL por muestra a procesar, por lo que se deben cargar originalmente un poco más de estos reactivos
     
+    s20.flow_rate.aspirate = 100
+    s20.flow_rate.dispense = 100
+    s20.flow_rate.blow_out = 3
+    
+    s20.pick_up_tip()
+    s20.mix(5, 20, primer_h2o) # Mixing the eppendorf with primers+h2o 5 times with 20 uL
+    
     s20.distribute(3,
-                    primer_h2o, # From
-                    [o_plate.wells()[:sample_number]], #To
-                    mix_before = (3, 20), # Mixing vol_primers_h2o 5 times
-                    blow_out = True,
-                    blowout_location = 'source well',
-                    carryover = True # Split volumes when vol > pipette.max_volume
-                   )
+                   primer_h2o, # From
+                   [o_plate.wells()[:sample_number]], #To
+                   new_tip = 'never',
+                   blow_out = True,
+                   blowout_location = 'source well',
+                   carryover = True # Split volumes when vol > pipette.max_volume
+                  )
+    
+    s20.drop_tip()
     
     
     
     
     # RT - PASO 2: RNA
+    volumen_templado = 2 #uL
+    
     m20.flow_rate.aspirate = 20
     m20.flow_rate.dispense = 20
-    
-    volumen_templado = 2 #uL
+    m20.flow_rate.blow_out = volumen_templado
     
     output_samples = [col for col in o_plate.rows()[0][:col_number]]
     
@@ -102,11 +112,10 @@ def run(protocol):
         m20.pick_up_tip()
         m20.mix(3, 20, rna_sample)
         m20.aspirate(volumen_templado, rna_sample)
-        m20.dispense(m20.current_volume, output_sample.top(z=-5))
+        m20.dispense(m20.current_volume, output_sample)
         
-        # Puede que se elimine el blow_out y touch_tip
-        m20.blow_out(output_sample.top(z=-5))
-        m20.touch_tip(output_sample, v_offset = -0.5, speed = 50)
+        m20.blow_out(output_sample.bottom(z=5))
+        #m20.touch_tip(output_sample, v_offset = -0.5, speed = 50)
         
         m20.drop_tip()
         
@@ -128,16 +137,22 @@ def run(protocol):
     
     
     # RT - PASO 4: Master Mix
-    s20.flow_rate.aspirate = 5
-    s20.flow_rate.dispense = 5
-    
     volumen_mastermix = 5 #uL
     
     output_samples = o_plate.wells()[:sample_number]
+    
+    s20.pick_up_tip()
+    s20.mix(5, 20, master_mix)
+    s20.drop_tip()
+    
+    s20.flow_rate.aspirate = 5
+    s20.flow_rate.dispense = 5
+    s20.flow_rate.blow_out = 5
     
     for output_sample in output_samples:
         s20.transfer(volumen_mastermix,
                      master_mix,
                      output_sample,
+                     new_tip = 'always',
                      blow_out = True,
                      blowout_location = 'destination well')
