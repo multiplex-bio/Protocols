c60f1fe6a21fae1057e4e4ced641165f7c337dd0
bpvalderrama <bvalderrama@multiplex.bio>
2021-12-01 12:10:02 -0300

    New approach to calculate final volumes for each reagent

--- RT/carga_rx_RTprep.py
+++ RT/carga_rx_RTprep.py
@@ -7,35 +7,41 @@ is_number = False
 
 while not is_number:
     try:
-        sample_number = int(input("\nIngresa la cantidad de muestras que quieres procesar (considera los 4 controles como muestras): "))
+        sample_number = int(input("\nIngresa la cantidad de muestras que quieres procesar (considera los controles de extracción como muestras): "))
         
-        if sample_number >= 1 and sample_number <= 94:
+        if sample_number >= 1 and sample_number <= 96:
             is_number = True
             
         else:
-            print("\nDebes ingresar un número entre 1 y 94")
+            print("\nDebes ingresar un número entre 1 y 96")
             
     except ValueError:
-        print("\nDebes ingresar un número entre 1 y 94")
+        print("\nDebes ingresar un número entre 1 y 96")
     
 
-    
+# Volume calculation for the reagents used in the RT...
+
+vol_h2o = round(2.375 * (sample_number + 5), 1) # 2.375 Proportion of H2O for each 3 uL of H2O+Primers used in the mastermix
+vol_primers = round(0.625 * (sample_number + 5), 1) # 0.625 Proportion of H2O for each 3 uL of H2O+Primers used in the mastermix
+
+# Additionally, we need 5 uL of mastermix for each sample, so we will use
+vol_mastermix = (sample_number+5)*5 # We load an extra of mastermix.
     
 
     
 # Script's output ...
 
 print("\n\n-> Para preparar el mix de agua y primers, debes cargar los siguientes volumenes en un tubo eppendorf de 1.5 mL: ")
-print("\n\t{} uL de H2O nuclease free".format(vol_h2o_my_samples))
-print("\n\t{} uL de primers".format(vol_primers_my_samples))
+print("\n\t{} uL de H2O nuclease free".format(vol_h2o))
+print("\n\t{} uL de primers".format(vol_primers))
 print("\n\tEl eppendorf con H2O free y Primers se debe colocar en la posición A1 del rack")
 
 
 print("\n\n-> Adicionalmente, en otro tubo eppendorf de 1.5 mL, debes agregar: ")
-print("\n\t{} uL de Master Mix 2x".format(vol_mastermix_my_samples))
+print("\n\t{} uL de Master Mix 2x".format(vol_mastermix))
 print("\n\tEl eppendorf con Master Mix 2x se debe colocar en la posición B1 del rack\n")
 
 
 print("\n\n-> Finalmente, debes agregar: ")
-print("\n\t50 uL de Control + en un eppendorf, que se debe colocar en la posición D6 del rack\n")
-print("\t50 uL de Control - en un eppendorf, que se debe colocar en la posición C6 del rack\n")
+print("\n\t25 uL de Control + en un eppendorf, que se debe colocar en la posición D6 del rack\n")
+print("\t25 uL de Control - en un eppendorf, que se debe colocar en la posición C6 del rack\n")
