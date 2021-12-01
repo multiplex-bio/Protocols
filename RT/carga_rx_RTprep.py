c10055fdb2c59f3e342e785e27bb3b40605ed8e5
bpvalderrama <bvalderrama@multiplex.bio>
2021-12-01 09:22:28 -0300

    New message for the output with nre possitions for the controls

--- RT/carga_rx_RTprep.py
+++ RT/carga_rx_RTprep.py
@@ -21,28 +21,9 @@ while not is_number:
 
     
     
-## Calculations of the volumes we gonna need ...
 
-
-# We need a mix h2o and primers:
-vol_96_samples = 335 # uL needed to analyze 96 samples
-vol_my_samples = round((vol_96_samples * (sample_number + 2)) / 96, 2) # uL needed the samples I want to analyze
-
-# The mix with primers and h2o nuclease free has the following proportions:
-h2o_prop = 11/12
-primers_prop = 1/12
-
-# So, for the mix of primers and h2o, we will need:
-vol_h2o_my_samples = round(vol_my_samples * h2o_prop, 2)
-vol_primers_my_samples = round(vol_my_samples * primers_prop, 2)
-
-# Additionally, we need 5 uL of mastermix for each sample, so we will use
-vol_mastermix_my_samples = (sample_number+5)*5 # We load an extra of mastermix.
-
-
-
-
-# Program's output ...
+    
+# Script's output ...
 
 print("\n\n-> Para preparar el mix de agua y primers, debes cargar los siguientes volumenes en un tubo eppendorf de 1.5 mL: ")
 print("\n\t{} uL de H2O nuclease free".format(vol_h2o_my_samples))
@@ -50,11 +31,11 @@ print("\n\t{} uL de primers".format(vol_primers_my_samples))
 print("\n\tEl eppendorf con H2O free y Primers se debe colocar en la posición A1 del rack")
 
 
-print("\n\n-> Adicionalmente, debes agregar: ")
-print("\n\t50 uL de Control + en un eppendorf, que se debe colocar en la posición B1 del rack\n")
-print("\t50 uL de Control - en un eppendorf, que se debe colocar en la posición C1 del rack\n")
-
-
-print("\n\n-> Finalmente, en otro tubo eppendorf de 1.5 mL, debes agregar: ")
+print("\n\n-> Adicionalmente, en otro tubo eppendorf de 1.5 mL, debes agregar: ")
 print("\n\t{} uL de Master Mix 2x".format(vol_mastermix_my_samples))
-print("\n\tEl eppendorf con Master Mix 2x se debe colocar en la posición D1 del rack\n")
+print("\n\tEl eppendorf con Master Mix 2x se debe colocar en la posición B1 del rack\n")
+
+
+print("\n\n-> Finalmente, debes agregar: ")
+print("\n\t50 uL de Control + en un eppendorf, que se debe colocar en la posición D6 del rack\n")
+print("\t50 uL de Control - en un eppendorf, que se debe colocar en la posición C6 del rack\n")
